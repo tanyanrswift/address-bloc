011140b261974f8510667f4efe0029cde963c38c
Tanya Swift <tanyanicolerussi@gmail.com>
2019-01-29 11:56:59 -0700

    Checkpoint Testing complete

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -59,4 +59,8 @@ module.exports = class MenuController{
     console.log("Thanks for using AddressBloc!");
     process.exit();
   }
+
+  getContactCount(){
+    return this.contacts.length;
+  }
 }
